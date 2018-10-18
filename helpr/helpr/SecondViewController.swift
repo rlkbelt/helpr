94b8237b0524206bf181b43988ce515107634870
ryan.konynenbelt <ryan.konynenbelt@mac06-wd.local>
2018-10-18 14:07:29 -0600

    First commit, no functionality

---
+++ helpr/helpr/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  helpr
+//
+//  Created by ryan.konynenbelt on 2018-10-18.
+//  Copyright © 2018 ryan.konynenbelt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
