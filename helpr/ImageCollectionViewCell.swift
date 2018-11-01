39ea0ff4ea12ad305f54cc84e5160bb1c4267913
walter.alvarez <walter.alvarez@mac06-ea.local>
2018-11-01 10:56:20 -0600

    collectionviewCameraPermissions

---
+++ helpr/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  helpr
+//
+//  Created by walter.alvarez on 2018-10-31.
+//  Copyright © 2018 ryan.konynenbelt. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postImg: UIImageView!
+}
