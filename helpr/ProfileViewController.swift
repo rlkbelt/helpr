b2fa43632c7d358de8f9b5750fefff11a4ee0667
walter.alvarez <walter.alvarez@mac06-ea.local>
2018-11-14 19:15:18 -0700

    new account pages (there's a lot fo them, thanks Chris)

---
+++ helpr/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  helpr
+//
+//  Created by walter.alvarez on 2018-11-14.
+//  Copyright © 2018 ryan.konynenbelt. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var ivProfilePic: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblRating: UILabel!
+    @IBOutlet weak var lblJobCount: UILabel!
+    @IBOutlet weak var btnSkills: UIButton!
+    @IBOutlet weak var tvReviews: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ivProfilePic.layer.cornerRadius = ivProfilePic.frame.width / 2
+        ivProfilePic.layer.borderWidth = 1
+        ivProfilePic.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
