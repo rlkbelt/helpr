3524a6918351ad917707741665065f35d18a2ea1
walter.alvarez <walter.alvarez@mac06-ea.local>
2018-11-23 17:12:18 -0700

    create post redesign

--- helpr/helpr/Controllers/PostAdTableViewController.swift
+++ helpr/helpr/Controllers/PostAdTableViewController.swift
@@ -9,20 +9,18 @@
 import UIKit
 import os.log
 
-class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PostAdTableViewController: UITableViewController, UITextViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
-    @IBOutlet weak var tvDescription: UITextView!
-    @IBOutlet weak var tfCategory: UITextField!
+    @IBOutlet weak var lCategory: UILabel!
     @IBOutlet weak var tfTitle: UITextField!
-    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var tvDescription: UITextView!
     @IBOutlet weak var tfTags: UITextField!
-    @IBOutlet weak var saveBtn: UIBarButtonItem!
+    @IBOutlet weak var postBtn: UIBarButtonItem!
     @IBOutlet weak var cvPhotos: UICollectionView!
     @IBOutlet var tgrPhotos: UITapGestureRecognizer!
     
     var job: Job?
-    let categories = ["Assembly", "Cleaning", "General", "Minor Repair", "Technology","Tutoring"]
     var postPhotos = [UIImage]() //allow update of UICollectionViewCells
     var indexPathForCell : IndexPath = [] //variable to allow updating of photos
     var customPhotoAdded = false;
@@ -31,75 +29,23 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        
-        tfCategory.inputView = pickerView
-        tvDescription.layer.borderColor = UIColor.lightGray.cgColor
-        tvDescription.layer.borderWidth = 1
         tvDescription.delegate = self
-        
         postPhotos.insert(UIImage(named: "defaultPhoto")!, at: 0)
     }
     
-    //unsure what this does but part of solution to scroll view up when textfield is selected but would normally be cut off by keyboard
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return true
-    }
-    
-    //function called when need to move view up to prevent textfield from being covered by keyboard
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        animateViewMoving(up: true, moveValue: 200)
-    }
-    
-    //function called to move view down once field is no longer being edited and keyboard is dismissed
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        animateViewMoving(up: false, moveValue: 200)
-    }
-    
-    //function that actually animates the movement of view when textview would normally be covered by keyboard
-    func animateViewMoving (up:Bool, moveValue :CGFloat){
-        let movementDuration:TimeInterval = 0.3
-        let movement:CGFloat = ( up ? -moveValue : moveValue)
-        UIView.beginAnimations( "animateView", context: nil)
-        UIView.setAnimationBeginsFromCurrentState(true)
-        UIView.setAnimationDuration(movementDuration )
-        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
-        UIView.commitAnimations()
-    }
-    
-    //this code allows keyboard to resign when user touches outside of field or hides the pickerView in the case of the category
+    //this code allows keyboard to resign when user touches outside of field
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-        if (pickerView.isHidden == false) {
-            pickerView.isHidden = true;
-        }
-    }
-    
-    //when category textfield is tapped show pickerView with category options
-    @IBAction func categoryClick(_ sender: UITextField) {
-        print("Opening Categories pickerView")
-        pickerView.isHidden = false
-        tfCategory.resignFirstResponder()
-        pickerView.becomeFirstResponder()
-        return
-    }
-    
-    //when pickerView makes selection this is called to hide pickerView
-    @IBAction func categoryDoneEdit(_ sender: UITextField) {
-        pickerView.isHidden = true
-        sender.resignFirstResponder()
-        return
+
     }
     
     //on cancel we must restore every field to startup values since there is no 'Back' button to handle this
     @IBAction func exitPostAd(_ sender: UIBarButtonItem) {
-        tfCategory.text = ""
-        tfTitle.text = ""
+        lCategory.text = "No Category Selected"
+        tfTitle.placeholder = "No Title Provided"
         tvDescription.textColor = UIColor.lightGray
-        tvDescription.text = "No description provided"
-        tfTags.text = ""
+        tvDescription.text = "Enter your post description here"
+        tfTags.placeholder = "#sampleTag"
         postPhotos.removeAll()
         postPhotos.insert(UIImage(named: "defaultPhoto")!, at: 0)
         self.cvPhotos.reloadData()
@@ -111,10 +57,10 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
     @IBAction func finishAddingPost(_ sender: UIBarButtonItem) {
         if (sender.title == "Done") {
             tvDescription.resignFirstResponder()
-            saveBtn.title = "Save"
+            postBtn.title = "Post"
         }
         else {
-            let category = tfCategory.text
+            let category = lCategory.text
             let title = tfTitle.text ?? "Untitled Post"
             let description = tvDescription.text ?? "No description provided"
             let tags = tfTags.text ?? ""
@@ -127,7 +73,7 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
                 
                 NotificationCenter.default.post(name: NSNotification.Name(rawValue: "loadJobs"), object: nil)
                 
-                exitPostAd(saveBtn)
+                exitPostAd(postBtn)
                 tabBarController?.selectedIndex = 0
             }
                 //title or category were not provided
@@ -143,7 +89,6 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
     @IBAction func tagEdit(_ sender: UITextField) {
         print("Entered tagEdit")
         tfTags.becomeFirstResponder()
-        textFieldDidBeginEditing(tfTags)
     }
     
     @IBAction func tagExit(_ sender: UITextField) {
@@ -154,7 +99,6 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
     @IBAction func tagFinish(_ sender: UITextField) {
         print("Exit tag Edit")
         tfTags.resignFirstResponder()
-        textFieldDidEndEditing(tfTags)
     }
     
     @IBAction func fieldExit(_ sender: Any) {
@@ -169,18 +113,18 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
         print("Ending field edit")
         //self.view.endEditing(true)
     }
-    
+
     //clear 'placeholder' text and change Save button behaviour
     func textViewDidBeginEditing(_ textView: UITextView) {
         print("Description field selected for editing")
-        if (tvDescription.text == "No description provided") {
+        if (tvDescription.text == "Enter your post description here") {
             tvDescription.text = ""
         }
         tvDescription.textColor = UIColor.black
-        saveBtn.title = "Done"
+        postBtn.title = "Done"
         
     }
-    
+ 
     //limit character count in Description field
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
@@ -193,9 +137,9 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
         print("Description field done editing")
         if (tvDescription.text == "") {
             tvDescription.textColor = UIColor.lightGray
-            tvDescription.text = "No description provided"
+            tvDescription.text = "Enter your post description here"
         }
-        saveBtn.title = "Save"
+        postBtn.title = "Post"
     }
     
     //ensures that when a photo is added or changed there is another photo that explicitly shows the add photo
@@ -221,13 +165,12 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
     //initializes photo collectionview to images stored in postPhotos array, on startup it is just one instance of defaultPhoto
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
-        
         cell.postImg.image = postPhotos[indexPath.row]
-        
+        print("In here")
         return cell
     }
     
-    //when a collectionViewCell is selected this funcion is called to open photo library. Consider editing this function to allow user to take picture at runtime. need to investigate if user discarding photo is handled by system or developer will need to handle that event
+    //when a collectionViewCell is selected this function is called to open photo library. Consider editing this function to allow user to take picture at runtime. need to investigate if user discarding photo is handled by system or developer will need to handle that event
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         indexPathForCell = indexPath
@@ -266,64 +209,5 @@ class PostAdViewController: UIViewController, UITextViewDelegate, UIPickerViewDe
         // Dismiss the picker.
         dismiss(animated: true, completion: nil)
     }
-    
-    
-    //MARK: Actions
-    //MARK: UIPickerView Delegate Methods
-    
-    //returns number of components in pickerView. we currently only have one componenent with category.count rows
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return categories.count
-    }
-    
-    //title for row of each pickerView is set to value in categories array for that row value
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return categories[row]
-    }
-    
-    //Function changes category textField text to whatever was selected by picker
-    //If the only picture in collectionView is defaultPhoto, change to default for that category
-    //Add new defaultPhoto to allow addition of more photos (tapRecognizer not working)
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        tfCategory.text = categories[row]
-        tfCategory.resignFirstResponder()
-        pickerView.isHidden = true;
-        if (!customPhotoAdded) {
-            print("Only default item in postPhotos")
-            switch tfCategory.text {
-            case "Cleaning":
-                postPhotos[0] = UIImage(named: "CleanDefault")!
-            case "Technology":
-                postPhotos[0] = UIImage(named: "TechDefault")!
-            case "Tutoring":
-                postPhotos[0] = UIImage(named: "TutorDefault")!
-            default:
-                print("Unhandled Case")
-            }
-        }
-        checkAddPhoto()
-        self.cvPhotos.reloadData()
-        return
-    }
-    
-    //changes default attributes of pickerView to add text color, background color and more
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        var pickerLabel = view as! UILabel!
-        if view == nil {  //if no label there yet
-            pickerLabel = UILabel()
-            //color the label's background
-            //pickerLabel!.backgroundColor = UIColor.lightGray
-        }
-        let titleData = categories[row]
-        let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 26),NSAttributedString.Key.foregroundColor:UIColor(named: "RoyalPurple")!])
-        pickerLabel!.attributedText = myTitle
-        pickerLabel!.textAlignment = .center
-        
-        return pickerLabel!
-    }
+
 }
